e074cc73ba3ecd2bffd55e721c71fc50bc2fc913
Jonas Triki <kosi_722@hotmail.com>
2018-08-04 02:24:37 +0200

    Styled toilets, added launcher-icons and location redux state

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -1,20 +1,31 @@
 import 'package:dokart/models/toilet.dart';
+import 'package:latlong/latlong.dart';
 import 'package:meta/meta.dart';
 
 @immutable
 class AppState {
   final bool isLoading;
   final dynamic error;
+  final LatLng location;
   final List<Toilet> toilets;
 
-  AppState({this.isLoading = false, this.error, this.toilets = const []});
+  AppState(
+      {this.isLoading = false,
+      this.error,
+      this.location,
+      this.toilets = const []});
 
   factory AppState.loading() => AppState(isLoading: true);
 
-  AppState copyWith({bool isLoading, dynamic error, List<Toilet> toilets}) =>
+  AppState copyWith(
+          {bool isLoading,
+          dynamic error,
+          LatLng location,
+          List<Toilet> toilets}) =>
       AppState(
           isLoading: isLoading ?? this.isLoading,
           error: error ?? this.error,
+          location: location ?? this.location,
           toilets: toilets ?? this.toilets);
 
   @override
@@ -24,13 +35,18 @@ class AppState {
           runtimeType == other.runtimeType &&
           isLoading == other.isLoading &&
           error == other.error &&
+          location == other.location &&
           toilets == other.toilets;
 
   @override
-  int get hashCode => isLoading.hashCode ^ error.hashCode ^ toilets.hashCode;
+  int get hashCode =>
+      isLoading.hashCode ^
+      error.hashCode ^
+      location.hashCode ^
+      toilets.hashCode;
 
   @override
   String toString() {
-    return 'AppState{isLoading: $isLoading, error: $error, toilets: $toilets}';
+    return 'AppState{isLoading: $isLoading, error: $error, location: $location, toilets: $toilets}';
   }
 }
---
+++ lib/screens/toilets/widgets/toilet_card.dart
@@ -0,0 +1,142 @@
+import 'package:dokart/models/meta_state.dart';
+import 'package:dokart/models/toilet.dart';
+import 'package:flutter/material.dart';
+
+Color _iconColor = Color(0xff35485e);
+
+Widget _top(BuildContext context, Toilet toilet) => Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 2,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                toilet.getDistance,
+                textScaleFactor: 1.5,
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .body1
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                toilet.getAdresse.trim().length > 0
+                    ? toilet.getAdresse
+                    : toilet.getPlassering,
+                textScaleFactor: 0.9,
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: MaterialButton(
+              onPressed: () {},
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.map,
+                    color: _iconColor,
+                  ),
+                  Text(
+                    "Åpne i kart",
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              )),
+        )
+      ],
+    );
+
+Widget _infoItem(BuildContext context, IconData iconData, String text) =>
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          iconData,
+          color: _iconColor,
+        ),
+        Text(
+          text,
+          textScaleFactor: 0.9,
+        )
+      ],
+    );
+
+Widget _bottom(BuildContext context, Toilet toilet) {
+  final List<Widget> additionalInfoItems = [];
+  if (toilet.isRullestol == MetaState.YES) {
+    additionalInfoItems.add(_infoItem(context, Icons.accessible, "Rullestom"));
+  }
+  if (toilet.isStellerom == MetaState.YES) {
+    additionalInfoItems.add(_infoItem(
+        context, const IconData(0xf06c, fontFamily: "mdi"), "Stellerom"));
+  }
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: <Widget>[
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Icon(
+              Icons.access_time,
+              color: _iconColor,
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                toilet.getTidHverdag,
+                textScaleFactor: 0.9,
+              ),
+              Text(
+                toilet.getTidLordag,
+                textScaleFactor: 0.9,
+              ),
+              Text(
+                toilet.getTidSondag,
+                textScaleFactor: 0.9,
+              ),
+            ],
+          )
+        ],
+      ),
+      _infoItem(context, Icons.monetization_on,
+          toilet.getPris > 0 ? toilet.getPris.toString() + " kr" : "Gratis")
+    ]..addAll(additionalInfoItems),
+  );
+}
+
+Card toiletCard(BuildContext context, Toilet toilet, onClick) => Card(
+      elevation: 2.0,
+      child: InkWell(
+        onTap: () {
+          onClick();
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            height: 120.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  child: _top(context, toilet),
+                ),
+                Expanded(
+                  child: _bottom(context, toilet),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
